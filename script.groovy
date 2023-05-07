be45e1c17ae9b43633fbfbffbd442ae890d84324
Sebastian <sebastian.stemmer@dig-it-up.de>
2023-05-07 18:41:33 +0200

    Yet another fix again

--- script.groovy
+++ script.groovy
@@ -10,6 +10,7 @@ def buildImage() {
         // sh "docker login -u $USER -p $PASS 681800194367.dkr.ecr.eu-north-1.amazonaws.com/new-app:$BUILD_NUMBER"
         // sh "docker push $USER/$JOB_NAME:$BUILD_NUMBER"
         // }
+        sh "aws --version"
         sh "aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/v8z9z5a4"
         sh "docker build -t $JOB_NAME:$BUILD_NUMBER ."
         sh "docker tag $JOB_NAME:$BUILD_NUMBER public.ecr.aws/v8z9z5a4/new-app:$BUILD_NUMBER"
