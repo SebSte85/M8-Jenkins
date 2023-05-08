e917b85b5706e43c33e4d3d127e32c7de0b2f9ea
Sebastian <sebastian.stemmer@dig-it-up.de>
2023-05-08 16:09:58 +0200

    Added Dockerfile

--- script.groovy
+++ script.groovy
@@ -5,6 +5,7 @@ def buildJar(){
 
 def buildImage() {
     echo 'Building the docker image...'
+<<<<<<< HEAD
         // withCredentials([aws(credentialsId: 'ec2-jenkins', accessKeyVariable:'AWS_ACCESS_KEY_ID', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY', region: 'eu-north-1')]) {
         // sh "docker build -t $USER/$JOB_NAME:$BUILD_NUMBER ."
         // sh "docker login -u $USER -p $PASS 681800194367.dkr.ecr.eu-north-1.amazonaws.com/new-app:$BUILD_NUMBER"
@@ -15,6 +16,19 @@ def buildImage() {
         sh "docker build -t $JOB_NAME:$BUILD_NUMBER ."
         sh "docker tag $JOB_NAME:$BUILD_NUMBER public.ecr.aws/v8z9z5a4/new-app:$BUILD_NUMBER"
         sh "docker push public.ecr.aws/v8z9z5a4/new-app:$BUILD_NUMBER"
+=======
+        withCredentials([aws(credentialsId: 'ec2-jenkins', accessKeyVariable:'AWS_ACCESS_KEY_ID', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY', region: 'eu-north-1')]) {
+        sh "aws --version"
+        sh "docker build -t $USER/$JOB_NAME:$BUILD_NUMBER ."
+        sh "docker login -u $USER -p $PASS 681800194367.dkr.ecr.eu-north-1.amazonaws.com/new-app:$BUILD_NUMBER"
+        sh "docker push $USER/$JOB_NAME:$BUILD_NUMBER"
+        }
+        // sh "aws --version"
+        // sh "aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/v8z9z5a4"
+        // sh "docker build -t $JOB_NAME:$BUILD_NUMBER ."
+        // sh "docker tag $JOB_NAME:$BUILD_NUMBER public.ecr.aws/v8z9z5a4/new-app:$BUILD_NUMBER"
+        // sh "docker push public.ecr.aws/v8z9z5a4/new-app:$BUILD_NUMBER"
+>>>>>>> 54eb7ac (Yet another fix again)
 }   
 
 def deployApp(){
