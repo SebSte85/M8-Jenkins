5bf7a1425a7673f5a142ed4ee2ed050ac0018fc5
Sebastian <sebastian.stemmer@dig-it-up.de>
2023-05-09 12:17:19 +0200

    Removed tags

--- script.groovy
+++ script.groovy
@@ -7,8 +7,8 @@ def buildImage() {
     echo 'Building the docker image...'
         sh "aws --version"
         sh "aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/v8z9z5a4"
-        sh "docker build -t $JOB_NAME:$BUILD_NUMBER-$GIT_BRANCH ."
-        sh "docker tag $JOB_NAME:$BUILD_NUMBER-$GIT_BRANCH public.ecr.aws/v8z9z5a4/new-app:$BUILD_NUMBER"
+        sh "docker build -t $JOB_NAME:$BUILD_NUMBER ."
+        sh "docker tag $JOB_NAME:$BUILD_NUMBER public.ecr.aws/v8z9z5a4/new-app:$BUILD_NUMBER"
         sh "docker push public.ecr.aws/v8z9z5a4/new-app:$BUILD_NUMBER"
 }   
 
