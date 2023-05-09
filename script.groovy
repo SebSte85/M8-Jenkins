a127fee37ed74c7381339a2c7a08eb038983f13c
Sebastian <sebastian.stemmer@dig-it-up.de>
2023-05-09 15:23:47 +0200

    Added automatic increment of version in pom.xml

--- script.groovy
+++ script.groovy
@@ -1,6 +1,7 @@
 def buildJar(){
     echo 'Building the application jar...'
     sh "mvn clean package"
+    sh "mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion} versions:commit"
 }
 
 def buildImage() {
